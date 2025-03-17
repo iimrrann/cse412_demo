976cdb98ce3efb82d1b811253bf4b53059eb19ec
iimrrann <159030415+iimrrann@users.noreply.github.com>
2025-03-17 21:09:13 +0600

    Update app.py
    importing numpy

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session, jsonify
 import sqlite3
+import numpy as np
 import os
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
